1e6422753878fb5cc4a2fdeccfead1d6bfd1ae02
sukrutan1 <sukrutan@posta.mu.edu.tr>
2026-07-05 17:54:36 +0300

    created message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 const listen = async () => {
   try {
     await connectDB();
