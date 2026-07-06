67e4e393834e7af8f51f063b6512a99a1623f82f
sukrutan1 <sukrutan@posta.mu.edu.tr>
2026-07-06 18:38:30 +0300

    frontend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhos:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 const listen = async () => {
